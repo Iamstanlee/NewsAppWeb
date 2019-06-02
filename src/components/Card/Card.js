ad77a4f3c79368970000fa6395c09f2f84fd4e59
Iamstanlee <stanlee.ak@gmail.com>
2019-06-02 08:33:19 +0100

    #Added Share to FB

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,7 +13,9 @@ const card = ({ title, time, featuredImage, description, url }) => {
       <img src={featuredImage} className={classes.featuredImage} alt={'Loading FeaturedImage...'} />
       <p>{description}</p>
       <div className={classes.actions}>
-        <Icon source={icons.share} style={{ marginLeft: '10px' }} />
+        <a href={'https://www.facebook.com/sharer.php?u=' + url}>
+          <Icon source={icons.share} style={{ marginLeft: '10px' }} />
+        </a>
         <a href={url}>
           <strong>Read More</strong>
         </a>
